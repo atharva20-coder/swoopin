90f49eb3c834972c9fa6cbc2490421edbef663e8
atharva20-coder <atharvajoshi2520@gmail.com>
2024-12-25 02:03:43 +0530

    initial release Open AI not working

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,14 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname:"scontent.cdninstagram.com"
+        hostname: 'scontent.cdninstagram.com', // Handles cdninstagram.com subdomains
+      },
+      {
+        protocol: 'https',
+        hostname: 'instagram.frpr5-1.fna.fbcdn.net', // Handles fbcdn.net subdomains
       },
     ],
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
