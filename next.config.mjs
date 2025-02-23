b4809dfe2e4c809bdf23a298d5d6be143266462b
atharva20-coder <atharvajoshi2520@gmail.com>
2025-02-24 00:49:30 +0530

    added all cdninstagram.com

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,14 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'scontent-iad3-2.cdninstagram.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'scontent-iad3-1.cdninstagram.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.cdninstagram.com',
+      },
     ],
   },
 };
